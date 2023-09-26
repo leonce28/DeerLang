e4bf89a1e29198b33b74e8aa987e5ef6f8abe44b
leonce <leoncelei28@gmail.com>
2023-09-26 10:39:43 +0800

    write asm file content complete.

--- source/common/funcs.c
+++ source/common/funcs.c
@@ -440,138 +440,138 @@ int file_read_content(const char *file_path, char **str)
 
 int file_write_content(const DeerLinkedList *codes, const char *asm_file)
 {
-    // FILE *fp;
-    // int idx, line_len, data_len;
-    // char line[VAR_OFFSET_MAX * 2];
-    // char *data = nullptr;
+    int data_len = 0;
+    int line_len = 0;
+    FILE *fp = nullptr;
+    Code *code = nullptr;
+    char *data = nullptr;
+    char line[VAR_OFFSET_MAX * 2];
 
-    // if (codes == nullptr || asm_file == nullptr) {
-    //     return CMM_FAILED;
-    // }
+    if (!codes || codes->size == 0 || !asm_file) {
+        return CMM_FAILED;
+    }
 
-    // if (strlen(asm_file) == 0) {
-    //     asm_file = DEFAULT_ASM_PATH;
-    // }
+    if (strlen(asm_file) == 0) {
+        asm_file = DEFAULT_ASM_PATH;
+    }
 
-    // data = (char *)malloc(codes->c_idx * (VAR_OFFSET_MAX * 2));
-    // data_len = 0;
+    data = (char *)malloc(codes->size * (VAR_OFFSET_MAX * 2));
+    assert(data);
 
-    // fp = fopen(asm_file, "w");
-    // if (fp == nullptr) {
-    //     printf("fopen file failed, file path: %s.\n", asm_file);
-    //     return CMM_FAILED;
-    // }
+    fp = fopen(asm_file, "w");
+    assert(fp);
 
-    // for (idx = 0; idx < codes->c_idx; ++idx) {
-    //     switch (codes->c[idx]->ins) {
-    //         case INS_LDC:
-    //             line_len = snprintf(line, VAR_OFFSET_MAX * 2, "LDC %s\n", codes->c[idx]->offset);
-    //             strncpy(data + data_len, line, line_len);
-    //             data_len += line_len;
-    //             break;
-    //         case INS_LD:
-    //             strcpy(data + data_len, "LD\n");
-    //             data_len += 3;
-    //             break;
-    //         case INS_ALD:
-    //             strcpy(data + data_len, "ALD\n");
-    //             data_len += 4;
-    //             break;
-    //         case INS_ST:
-    //             strcpy(data + data_len, "ST\n");
-    //             data_len += 3;
-    //             break;
-    //         case INS_AST:
-    //             strcpy(data + data_len, "__AST\n");
-    //             data_len += 6;
-    //             break;
-    //         case INS_PUSH:
-    //             strcpy(data + data_len, "PUSH\n");
-    //             data_len += 5;
-    //             break;
-    //         case INS_POP:
-    //             strcpy(data + data_len, "POP\n");
-    //             data_len += 4;
-    //             break;
-    //         case INS_JMP:
-    //             line_len = snprintf(line, VAR_OFFSET_MAX * 2, "JMP %s\n", codes->c[idx]->offset);
-    //             strncpy(data + data_len, line, line_len);
-    //             data_len += line_len;
-    //             break;
-    //         case INS_JZ:
-    //             line_len = snprintf(line, VAR_OFFSET_MAX * 2, "JZ %s\n", codes->c[idx]->offset);
-    //             strncpy(data + data_len, line, line_len);
-    //             data_len += line_len;
-    //             break;
-    //         case INS_ADD:
-    //             strcpy(data + data_len, "ADD\n");
-    //             data_len += 4;
-    //             break;
-    //         case INS_SUB:
-    //             strcpy(data + data_len, "SUB\n");
-    //             data_len += 4;
-    //             break;
-    //         case INS_MUL:
-    //             strcpy(data + data_len, "MUL\n");
-    //             data_len += 4;
-    //             break;
-    //         case INS_DIV:
-    //             strcpy(data + data_len, "DIV\n");
-    //             data_len += 4;
-    //             break;
-    //         case INS_LT:
-    //             strcpy(data + data_len, "LT\n");
-    //             data_len += 3;
-    //             break;
-    //         case INS_LE:
-    //             strcpy(data + data_len, "LE\n");
-    //             data_len += 3;
-    //             break;
-    //         case INS_GT:
-    //             strcpy(data + data_len, "GT\n");
-    //             data_len += 3;
-    //             break;
-    //         case INS_GE:
-    //             strcpy(data + data_len, "GE\n");
-    //             data_len += 3;
-    //             break;
-    //         case INS_EQ:
-    //             strcpy(data + data_len, "EQ\n");
-    //             data_len += 3;
-    //             break;
-    //         case INS_NE:
-    //             strcpy(data + data_len, "NE\n");
-    //             data_len += 3;
-    //             break;
-    //         case INS_IN:
-    //             strcpy(data + data_len, "IN\n");
-    //             data_len += 3;
-    //             break;
-    //         case INS_OUT:
-    //             strcpy(data + data_len, "OUT\n");
-    //             data_len += 4;
-    //             break;
-    //         case INS_ADDR:
-    //             line_len = snprintf(line, VAR_OFFSET_MAX * 2, "ADDR %s\n", codes->c[idx]->offset);
-    //             strncpy(data + data_len, line, line_len);
-    //             data_len += line_len;
-    //             break;
-    //         case INS_CALL:
-    //             line_len = snprintf(line, VAR_OFFSET_MAX * 2, "CALL %s\n", codes->c[idx]->offset);
-    //             strncpy(data + data_len, line, line_len);
-    //             data_len += line_len;
-    //             break;
-    //         case INS_RET:
-    //             strcpy(data + data_len, "RET\n");
-    //             data_len += 4;
-    //             break;
-    //         default:
-    //             invalid_call("output_asm_file");
-    //     }
-    // }
+    foreach (Code, code, codes) {
+        switch (code->ins) {
+            case INS_LDC:
+                line_len = snprintf(line, VAR_OFFSET_MAX * 2, "LDC %s\n", code->offset);
+                strncpy(data + data_len, line, line_len);
+                data_len += line_len;
+                break;
+            case INS_LD:
+                strcpy(data + data_len, "LD\n");
+                data_len += 3;
+                break;
+            case INS_ALD:
+                strcpy(data + data_len, "ALD\n");
+                data_len += 4;
+                break;
+            case INS_ST:
+                strcpy(data + data_len, "ST\n");
+                data_len += 3;
+                break;
+            case INS_AST:
+                strcpy(data + data_len, "__AST\n");
+                data_len += 6;
+                break;
+            case INS_PUSH:
+                strcpy(data + data_len, "PUSH\n");
+                data_len += 5;
+                break;
+            case INS_POP:
+                strcpy(data + data_len, "POP\n");
+                data_len += 4;
+                break;
+            case INS_JMP:
+                line_len = snprintf(line, VAR_OFFSET_MAX * 2, "JMP %s\n", code->offset);
+                strncpy(data + data_len, line, line_len);
+                data_len += line_len;
+                break;
+            case INS_JZ:
+                line_len = snprintf(line, VAR_OFFSET_MAX * 2, "JZ %s\n", code->offset);
+                strncpy(data + data_len, line, line_len);
+                data_len += line_len;
+                break;
+            case INS_ADD:
+                strcpy(data + data_len, "ADD\n");
+                data_len += 4;
+                break;
+            case INS_SUB:
+                strcpy(data + data_len, "SUB\n");
+                data_len += 4;
+                break;
+            case INS_MUL:
+                strcpy(data + data_len, "MUL\n");
+                data_len += 4;
+                break;
+            case INS_DIV:
+                strcpy(data + data_len, "DIV\n");
+                data_len += 4;
+                break;
+            case INS_LT:
+                strcpy(data + data_len, "LT\n");
+                data_len += 3;
+                break;
+            case INS_LE:
+                strcpy(data + data_len, "LE\n");
+                data_len += 3;
+                break;
+            case INS_GT:
+                strcpy(data + data_len, "GT\n");
+                data_len += 3;
+                break;
+            case INS_GE:
+                strcpy(data + data_len, "GE\n");
+                data_len += 3;
+                break;
+            case INS_EQ:
+                strcpy(data + data_len, "EQ\n");
+                data_len += 3;
+                break;
+            case INS_NE:
+                strcpy(data + data_len, "NE\n");
+                data_len += 3;
+                break;
+            case INS_IN:
+                strcpy(data + data_len, "IN\n");
+                data_len += 3;
+                break;
+            case INS_OUT:
+                strcpy(data + data_len, "OUT\n");
+                data_len += 4;
+                break;
+            case INS_ADDR:
+                line_len = snprintf(line, VAR_OFFSET_MAX * 2, "ADDR %s\n", code->offset);
+                strncpy(data + data_len, line, line_len);
+                data_len += line_len;
+                break;
+            case INS_CALL:
+                line_len = snprintf(line, VAR_OFFSET_MAX * 2, "CALL %s\n", code->offset);
+                strncpy(data + data_len, line, line_len);
+                data_len += line_len;
+                break;
+            case INS_RET:
+                strcpy(data + data_len, "RET\n");
+                data_len += 4;
+                break;
+            default:
+                invalid_call("output_asm_file");
+                break;
+        }
+    }
 
-    // fwrite(data, data_len, 1, fp);
-    // fclose(fp);
-    // free(data);
+    fwrite(data, data_len, 1, fp);
+    fclose(fp);
+    free(data);
     return CMM_SUCCESS;
 }
